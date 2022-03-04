cbb8a39a1ab1f757bf9ec7ee312cfe250196a00d
Siege333 <celiacreaghan@gmail.com>
2022-03-04 11:21:36 +0000

    Initialising the project folder to the repo

---
+++ src/main/java/com/qa/dvdLibrary/Dfesw10ProjectDvdLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.qa.dvdLibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dfesw10ProjectDvdLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dfesw10ProjectDvdLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qa/dvdLibrary/Dfesw10ProjectDvdLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.dvdLibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dfesw10ProjectDvdLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
