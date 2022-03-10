061c1904bdf91c87cc83ceacbd0dfec39d8ef75b
Siege333 <celiacreaghan@gmail.com>
2022-03-10 22:15:05 +0000

    added readAll integration testing including sql data and schema files

--- src/test/java/com/qa/dvdLibrary/controller/DvdControllerIntegrationTest.java
+++ src/test/java/com/qa/dvdLibrary/controller/DvdControllerIntegrationTest.java
@@ -1,9 +1,13 @@
 package com.qa.dvdLibrary.controller;
 
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +15,8 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.ResultMatcher;
@@ -20,6 +26,8 @@ import com.qa.dvdLibrary.entity.Dvd;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@Sql(scripts = {"classpath:dvd-schema.sql", "classpath:dvd-data.sql"}, 
+     executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
 @AutoConfigureMockMvc
 public class DvdControllerIntegrationTest {
 
@@ -35,7 +43,7 @@ public class DvdControllerIntegrationTest {
 		Dvd newDvd = new Dvd("Stardust", "Romance", 2007, 122, "PG", "Claire Danes", "Robert De Niro");
 		String newDvdJSON = this.mapper.writeValueAsString(newDvd);
 		
-		Dvd dvdSaved = new Dvd(1, "Stardust", "Romance", 2007, 122, "PG", "Claire Danes", "Robert De Niro");
+		Dvd dvdSaved = new Dvd(4, "Stardust", "Romance", 2007, 122, "PG", "Claire Danes", "Robert De Niro");
 		String dvdSavedJSON = this.mapper.writeValueAsString(dvdSaved);
 		//when
 		RequestBuilder request = post("/create")
@@ -48,4 +56,22 @@ public class DvdControllerIntegrationTest {
 		//then
 		this.mvc.perform(request).andExpect(responseStatus).andExpect(responseContent);
 	}
+	
+	@Test
+	void readAllDvdsTest() throws Exception {
+		List<Dvd> dvds = new ArrayList<>();
+		dvds.add(new Dvd(1, "Stardust", "Romance", 2007, 122, "PG", "Claire Danes", "Robert De Niro"));
+		dvds.add(new Dvd(2, "Twelve Monkeys", "Sci-Fi", 1995, 124, "15", "Medeleine Stowe", "Bruce Willis"));
+		dvds.add(new Dvd(3, "Hannibal", "Horror", 2001, 126, "18", "Julianne Moore", "Anthony Hopkins"));
+	
+		String savedDvdsJSON = this.mapper.writeValueAsString(dvds);
+		
+		RequestBuilder request = get("/readAll");
+		
+		ResultMatcher responseStatus = status().isFound();
+		ResultMatcher responseContent = content().json(savedDvdsJSON);
+		
+		this.mvc.perform(request).andExpect(responseStatus).andExpect(responseContent);
+	}
+	
 }
