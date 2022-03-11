01f637c39a70af225b3c559bd5d6cd5c7166e3b7
Siege333 <celiacreaghan@gmail.com>
2022-03-11 08:31:36 +0000

    added custom exception

--- src/main/java/com/qa/dvdLibrary/exceptions/DvdNotFoundException.java
+++ src/main/java/com/qa/dvdLibrary/exceptions/DvdNotFoundException.java
@@ -1,5 +1,18 @@
 package com.qa.dvdLibrary.exceptions;
 
-public class DvdNotFoundException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "No DVD entry with that ID")
+public class DvdNotFoundException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public DvdNotFoundException(String msg) {
+		super(msg);
+	}
+	
 }
